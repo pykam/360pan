8ac3ec6e5d79c2e59539ec8fac5b7bda89bbdbd4
Albert <qooal@yandex.ru>
2025-05-24 10:50:38 +0700

    minimal work version

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-// View360 is registered globally.
-const viewer = new View360("#viewer", {
-  // EquirectProjection and all other items are stored as properties within View360.
-  projection: new View360.EquirectProjection({
-    src: "URL_TO_IMAGE",
-    video: false
-  })
-});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // View360 is registered globally.
+    const viewer = new View360("#viewer", {
+    //EquirectProjection and all other items are stored as properties within View360.
+    projection: new View360. CylindricalProjection({
+        src: "images/IMG_9553.jpg",
+        partial: true,
+    })
+
+    });
+})
