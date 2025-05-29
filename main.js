41b2fd4b722bde9bb4374ececdb28c78656b4de8
Albert <qooal@yandex.ru>
2025-05-30 00:52:34 +0700

    Matched objects

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    const viewer = new View360("#viewer", {
-    initialYaw: 55,
-    projection: new View360. CylindricalProjection({
-        src: "images/IMG_9556.jpg",
-        partial: true,
-    })
-
+    const room1 = new View360("#room1", {
+        initialYaw: 55,
+        projection: new View360. CylindricalProjection({
+            src: "images/room1.jpg",
+            partial: true,
+        })
     });
-});
+
+    const room2 = new View360("#room2", {
+        initialYaw: 0,
+        projection: new View360.CylindricalProjection({
+            src: "images/room2.jpg",
+            partial: true,
+        })
+    });
+
+    }
+);
 
 const showInfo = document.querySelectorAll('.search-hotspot');
 const dialog = document.querySelector("dialog");
 const closeButton = document.querySelector(".btn-close");
+const info = document.querySelector(".info");
 
 for (let i = 0; i < showInfo.length; i++) {
     showInfo[i].addEventListener("click", () => {
         const img = document.querySelector("img");
         const imageName = showInfo[i].dataset.item;
         img.setAttribute("src", "images/" + imageName + ".jpg");
+        info.innerHTML = showInfo[i].querySelector(".desc").innerHTML;
         dialog.showModal();
     });
 }
