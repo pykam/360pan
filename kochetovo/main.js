f55f21cbb7d9405b7592ebc6566d664777a96f7f
Albert <qooal@yandex.ru>
2025-06-04 23:16:41 +0700

    Fix file structure and design

--- kochetovo/main.js
+++ kochetovo/main.js
@@ -1,20 +1,25 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    const room1 = new View360("#room1", {
-        initialYaw: 55,
-        projection: new View360. CylindricalProjection({
-            src: "images/room1.jpg",
-            partial: true,
-        })
-    });
 
-    const room2 = new View360("#room2", {
-        initialYaw: 0,
-        projection: new View360.CylindricalProjection({
-            src: "images/room2.jpg",
-            partial: true,
-        })
-    });
+    if (document.getElementById("room1")){
+        const room1 = new View360("#room1", {
+            initialYaw: 55,
+            projection: new View360. CylindricalProjection({
+                src: "images/room1.jpg",
+                partial: true,
+            })
+        });
+    }
+
+    if (document.getElementById("room2")){
+        const room2 = new View360("#room2", {
+            initialYaw: 0,
+            projection: new View360.CylindricalProjection({
+                src: "images/room2.jpg",
+                partial: true,
+            })
+        });
+    }
 
     }
 );
@@ -30,6 +35,7 @@ for (let i = 0; i < showInfo.length; i++) {
         const imageName = showInfo[i].dataset.item;
         img.setAttribute("src", "images/" + imageName + ".jpg");
         info.innerHTML = showInfo[i].querySelector(".desc").innerHTML;
+        document.body.classList.add("scroll-lock")
         dialog.showModal();
     });
 }
@@ -43,6 +49,7 @@ for (let i = 0; i < showInfo.length; i++) {
   });
 
 closeButton.addEventListener("click", () => {
+    document.body.classList.remove("scroll-lock")
     info.innerHTML = '';
     img.setAttribute("src", "");
     dialog.close();
